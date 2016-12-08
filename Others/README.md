a6c14ea0822a44844505ebf0f9861a9600331fa8
Jiangyiqun <diqiuyi2010@qq.com>
2016-12-08 10:19:20 +0800

    初始化

---
+++ Others/README.md
@@ -0,0 +1,5 @@
+这个文件夹与本项目无关，仅保存了一些设置 Nexus 6p （墙内）需要的文件。
+
+### 参考：
+- 一键安装应用：http://blog.csdn.net/tcpipstack/article/details/8698038
+- 网络验证服务器设置：http://bbs.gfan.com/android-8267219-1-1.html
---
+++ Others/修改网络验证服务器.bat
@@ -0,0 +1,5 @@
+﻿adb.exe shell settings put global captive_portal_use_https 1
+adb.exe shell settings put global captive_portal_server www.google.cn
+adb.exe shell settings put global captive_portal_http_url http://www.google.cn/generate_204
+adb.exe shell settings put global captive_portal_https_url https://www.google.cn/generate_204
+pause
