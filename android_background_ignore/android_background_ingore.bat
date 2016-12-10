afe5e380e13f62eb36796624cf29274462c86ba1
Jiangyiqun <diqiuyi2010@qq.com>
2016-12-10 18:46:17 +0800

    集成白名单到bat文件

--- android_background_ignore/android_background_ingore.bat
+++ android_background_ignore/android_background_ingore.bat
@@ -1,20 +1,11 @@
-﻿::名称 android_background_ignore
+::名称 android_background_ignore
 ::作者 白兔Jack
-::版本 V0.4
+::版本 V0.5
 ::项目地址 https://github.com/Jiangyiqun/android_background_ignore
-::注意 批处理必须用ANSI编码才能运行，否则会闪退
+::注意 本文件必须以 ANSI 编码保存
 
-::程序完整性检测
 @echo off
 cls
-if not exist white_list.txt (
-	echo.
-	echo 没有找到 white_list.txt 文件,请从如下地址下载
-	echo https://github.com/Jiangyiqun/android_background_ignore
-	echo 并拷贝至与 android_background_ignore.bat 相同路径下
-	echo 如果你不需要白名单，请按回车键跳过该步骤
-	pause
-)
 
 ::运行环境检测
 echo. 
@@ -36,7 +27,41 @@ if errorlevel 1 (
 )
 echo.
 
+::初次使用生成白名单
+if exist white_list.txt (
+	goto plan_menu
+)
+
+echo # 这是一份白名单，及其自身的撰写说明 > white_list.txt
+echo # 收录在此的应用，后台行为不会被限制 >> white_list.txt
+echo # >> white_list.txt
+echo # 撰写说明： >> white_list.txt
+echo # 一、依赖推送的应用，酌情添加至白名单 >> white_list.txt
+echo # 原因：被限制后台的应用，长时间不用后台服务会被关掉，无法接收推送 >> white_list.txt
+echo # 影响：即时通讯工具，如微信(无论是否使用GCM)；定时提醒工具，如滴答清单 >> white_list.txt
+echo # 建议：只添加依赖其推送的应用，不添加手动查看消息的应用 >> white_list.txt
+echo # >> white_list.txt
+echo # 二、依赖广播的应用，必须添加至白名单 >> white_list.txt
+echo # 原因：被限制后台的应用，无法接受系统广播，即无法随特定事件启动 >> white_list.txt
+echo # 影响：第三方来电提醒，短信验证码自动拾取，悬浮按钮，剪切板增强，手势增强 >> white_list.txt
+echo # 建议：通常应当添加至白名单 >> white_list.txt
+echo # >> white_list.txt
+echo # 三、以下应用，无需添加至白名单 >> white_list.txt
+echo # 系统内置应用，输入法，启动器（Launcher），由辅助功能启动的应用（如绿色守护） >> white_list.txt
+echo # >> white_list.txt
+echo # 请参考以上说明，删除多余的行，仅保留需要添加至白名单的应用 >> white_list.txt
+echo # >> white_list.txt
+echo # 白名单: >> white_list.txt
+adb shell pm list packages -3 > apps_list.txt
+for /f "tokens=2 delims=:" %%i in (apps_list.txt) do (
+	echo %%i >> white_list.txt
+	)
+del apps_list.txt
+echo 请阅读并编辑 white_list.txt 文件
+call white_list.txt
+
 ::用户菜单
+:plan_menu
 echo.
 echo 功能菜单：
 echo 1、限制自动唤醒
@@ -55,22 +80,12 @@ for /f "tokens=2 delims=:" %%i in (apps_list.txt) do (
 	adb shell cmd appops set %%i RUN_IN_BACKGROUND ignore
 )
 del apps_list.txt
-
 echo.
-echo 正在处理白名单(请忽略该步骤中的报错）：
-
-if exist white_list.txt (
-	echo.
-	for /f "eol=#" %%i in (white_list.txt) do (
-	echo 正在恢复 %%i 为默认状态
-	adb shell cmd appops set %%i RUN_IN_BACKGROUND allow
-	)
-)
-	
-if not exist white_list.txt (
-	echo.
-	echo 没有找到white_list.txt文件,跳过处理白名单
-	goto plan_end
+echo 正在处理白名单：
+echo.
+for /f "eol=#" %%i in (white_list.txt) do (
+echo 正在恢复 %%i 为默认状态
+adb shell cmd appops set %%i RUN_IN_BACKGROUND allow
 )
 goto plan_end
 
